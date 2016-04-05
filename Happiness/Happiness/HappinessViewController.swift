61f1af66eab0806e6c457b7ab85480a1f9d15005
Jian Shi <jians@usc.edu>
2016-04-04 21:40:18 -0700

    smile

---
+++ Happiness/Happiness/HappinessViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by 淡蓝色的泪 on 4/4/16.
+//  Copyright © 2016 University of Southern California. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+}
