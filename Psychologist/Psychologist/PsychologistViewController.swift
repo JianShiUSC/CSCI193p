e5fd31a57582d885161b5d4ea9165a51c4c1333a
Jian Shi <jians@usc.edu>
2016-04-19 00:10:26 -0700

    psychologist

---
+++ Psychologist/Psychologist/PsychologistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Psychologist
+//
+//  Created by 淡蓝色的泪 on 4/18/16.
+//  Copyright © 2016 University of Southern California. All rights reserved.
+//
+
+import UIKit
+
+class PsychologistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
