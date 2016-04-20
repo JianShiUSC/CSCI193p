b87eaf8bb16c9ed4339be4304b626cef2cad2791
Jian Shi <jians@usc.edu>
2016-04-20 00:24:28 -0700

    autolayout

---
+++ Autolayout/Autolayout/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Autolayout
+//
+//  Created by 淡蓝色的泪 on 4/19/16.
+//  Copyright © 2016 University of Southern California. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    
+}
+
