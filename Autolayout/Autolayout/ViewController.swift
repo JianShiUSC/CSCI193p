aa9c12ec3a938cfee117a800a932c1783a2f8c12
Jian Shi <jians@usc.edu>
2016-04-20 16:08:36 -0700

    autolayout

--- Autolayout/Autolayout/ViewController.swift
+++ Autolayout/Autolayout/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    @IBOutlet weak var loginField: UITextField!
     
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    var secure: Bool = false { didSet {updateUI() } }
+    
+    private func updateUI()
+    {
+        passwordField.secureTextEntry = secure
+        passwordLabel.text = secure ? "Secured Password" : "Password"
+    }
+    
+    @IBAction func toggleSecurity() {
+        secure = !secure
+    }
 }
 
