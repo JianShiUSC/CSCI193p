33b8fa9f50cf8a369764dfaa1a677172a4c6ba96
Jian Shi <jians@usc.edu>
2016-04-21 00:49:25 -0700

    cassini

---
+++ Cassini/Cassini/ImageViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by 淡蓝色的泪 on 4/20/16.
+//  Copyright © 2016 University of Southern California. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController, UIScrollViewDelegate
+{
+    var imageURL: NSURL? {
+        didSet {
+            image = nil
+            if view.window != nil {
+                fetchImage()
+            }
+        }
+    }
+    
+    private func fetchImage()
+    {
+        if let url = imageURL {
+            let imageData = NSData(contentsOfURL: url)
+            if imageData != nil {
+                image = UIImage(data: imageData!)
+            } else {
+                image = nil
+            }
+        }
+    }
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    {
+        didSet {
+            scrollView.contentSize = imageView.frame.size
+            scrollView.delegate = self
+            scrollView.minimumZoomScale = 0.03
+            scrollView.maximumZoomScale = 1.0
+        }
+    }
+    
+    func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    private var imageView = UIImageView()
+    
+    private var image: UIImage? {
+        get { return imageView.image }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView?.contentSize = imageView.frame.size
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.addSubview(imageView)
+//        if image == nil {
+//            imageURL = DemoURL.Stanford
+//        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil {
+            fetchImage()
+        }
+    }
+}
