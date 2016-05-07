dca7e127e2674f7a8ad4c3e0b1a00436794f04c1
Jian Shi <jians@usc.edu>
2016-05-07 09:26:53 -0700

    hackathon

---
+++ Hackathon/Trax/OurViewController.swift
@@ -0,0 +1,41 @@
+//
+//  OurViewController.swift
+//  Trax
+//
+//  Created by 淡蓝色的泪 on 5/6/16.
+//  Copyright © 2016 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class OurViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
