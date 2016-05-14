b8ac49e9c4caf837f05b467928dba979807ecbcf
Jian Shi <jians@usc.edu>
2016-05-14 16:04:35 -0700

    hackathon

--- Hackathon/Trax/LoginViewController.swift
+++ Hackathon/Trax/LoginViewController.swift
@@ -13,8 +13,6 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var userEmailTextField: UITextField!
     @IBOutlet weak var userPasswordTextField: UITextField!
     
-//    var storeVal: String? = "nil"
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -30,23 +28,65 @@ class LoginViewController: UIViewController {
         
         let userEmail = userEmailTextField.text
         let userPassword = userPasswordTextField.text
-//        storeVal = userEmailTextField.text!
         
-        let userEmailStored = NSUserDefaults.standardUserDefaults().stringForKey("userEmail")
-        let userPasswordStored = NSUserDefaults.standardUserDefaults().stringForKey("userPassword")
+//        let userEmailStored = NSUserDefaults.standardUserDefaults().stringForKey("userEmail")
+//        let userPasswordStored = NSUserDefaults.standardUserDefaults().stringForKey("userPassword")
+//        
+//        if userEmailStored == userEmail
+//        {
+//            if userPasswordStored == userPassword
+//            {
+//                // Login is successful
+//                NSUserDefaults.standardUserDefaults().setBool(true, forKey: "isUserLoggedIn")
+//                NSUserDefaults.standardUserDefaults().synchronize()
+//                self.dismissViewControllerAnimated(true, completion: nil)
+//                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//                let secondViewController = storyboard.instantiateViewControllerWithIdentifier("SecondVC") as UIViewController
+//                presentViewController(secondViewController, animated: true, completion: nil)
+//            }
+//        }
         
-        if userEmailStored == userEmail
-        {
-            if userPasswordStored == userPassword
-            {
-                // Login is successful
-                NSUserDefaults.standardUserDefaults().setBool(true, forKey: "isUserLoggedIn")
-                NSUserDefaults.standardUserDefaults().synchronize()
-                self.dismissViewControllerAnimated(true, completion: nil)
-                let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                let secondViewController = storyboard.instantiateViewControllerWithIdentifier("SecondVC") as UIViewController
-                presentViewController(secondViewController, animated: true, completion: nil)
+        if (userEmail!.isEmpty || userPassword!.isEmpty) { return }
+        
+        // Send user data to server side
+        let myUrl = NSURL(string: "http://www.shijianloveslareine.com/scripts/userLogin.php")
+        let request = NSMutableURLRequest(URL: myUrl!)
+        request.HTTPMethod = "POST"
+        
+        let postString = "email=\(userEmail)&password=\(userPassword)"
+        
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            if error != nil {
+                print("error=\(error)")
+                return
             }
+            
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as? NSDictionary
+                
+                if let parseJSON = json {
+                    var resultValue = parseJSON["status"] as! String!
+                    print("result: \(resultValue)")
+                    
+                    if (resultValue == "Success")
+                    {
+                        NSUserDefaults.standardUserDefaults().setBool(true, forKey: "isUserLoggedIn")
+                        NSUserDefaults.standardUserDefaults().synchronize()
+                        self.dismissViewControllerAnimated(true, completion: nil)
+                        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                        let secondViewController = storyboard.instantiateViewControllerWithIdentifier("SecondVC") as UIViewController
+                        self.presentViewController(secondViewController, animated: true, completion: nil)
+                    }
+                }
+                
+            } catch { print(error) }
+            
         }
+        task.resume()
     }
 }
--- Hackathon/Trax/RegisterPageViewController.swift
+++ Hackathon/Trax/RegisterPageViewController.swift
@@ -51,21 +51,77 @@ class RegisterPageViewController: UIViewController {
         
         
         
-        // Store data
-        NSUserDefaults.standardUserDefaults().setObject(userEmail, forKey: "userEmail")
-        NSUserDefaults.standardUserDefaults().setObject(userPassword, forKey: "userPassword")
-        NSUserDefaults.standardUserDefaults().synchronize()
-
+//        // Store data
+//        NSUserDefaults.standardUserDefaults().setObject(userEmail, forKey: "userEmail")
+//        NSUserDefaults.standardUserDefaults().setObject(userPassword, forKey: "userPassword")
+//        NSUserDefaults.standardUserDefaults().synchronize()
+//
+//        
+//        // Display alert message with confirmation
+//        let myAlert = UIAlertController(title: "Alert", message: "Registration is successful. Thank you!", preferredStyle: UIAlertControllerStyle.Alert)
+//        
+//        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default) { action in
+//            self.dismissViewControllerAnimated(true, completion: nil);
+//        }
+//        
+//        myAlert.addAction(okAction)
+//        self.presentViewController(myAlert, animated: true, completion: nil)
         
-        // Display alert message with confirmation
-        let myAlert = UIAlertController(title: "Alert", message: "Registration is successful. Thank you!", preferredStyle: UIAlertControllerStyle.Alert)
         
-        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default) { action in
-            self.dismissViewControllerAnimated(true, completion: nil);
+        // Send user data to server side
+        let myUrl = NSURL(string: "http://www.shijianloveslareine.com/scripts/userRegister.php")
+        let request = NSMutableURLRequest(URL: myUrl!)
+        request.HTTPMethod = "POST"
+        
+        let postString = "email=\(userEmail)&password=\(userPassword)"
+        
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            if error != nil {
+                print("error=\(error)")
+                return
+            }
+            
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as? NSDictionary
+                
+                if let parseJSON = json {
+                    var resultValue = parseJSON["status"] as! String!
+                    print("result: \(resultValue)")
+                    
+                    var isUserRegistered: Bool = false
+                    if (resultValue == "Success") {
+                        isUserRegistered = true
+                    }
+                    
+                    var messageToDisplay: String = parseJSON["message"] as! String!
+                    if (!isUserRegistered)
+                    {
+                        messageToDisplay = parseJSON["message"] as! String!
+                    }
+                    
+                    dispatch_async(dispatch_get_main_queue(), {
+                        
+                        var myAlert = UIAlertController(title: "Alert", message: messageToDisplay, preferredStyle: UIAlertControllerStyle.Alert)
+                        
+                        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default) { action in
+                            self.dismissViewControllerAnimated(true, completion: nil)
+                        }
+                        
+                        myAlert.addAction(okAction)
+                        self.presentViewController(myAlert, animated: true, completion: nil)
+                    })
+                }
+                
+            } catch { print(error)}
+            
         }
         
-        myAlert.addAction(okAction)
-        self.presentViewController(myAlert, animated: true, completion: nil)
+        task.resume()
         
     }
     
