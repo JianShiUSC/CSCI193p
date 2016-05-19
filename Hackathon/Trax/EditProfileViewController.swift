34167d281ee5994c1b77318f351b572378d174cd
Jian Shi <jians@usc.edu>
2016-05-19 01:20:15 -0700

    fix PHP session bugs

--- Hackathon/Trax/EditProfileViewController.swift
+++ Hackathon/Trax/EditProfileViewController.swift
@@ -36,5 +36,80 @@ class EditProfileViewController: UIViewController {
     }
 
     @IBAction func saveButtonTapped(sender: AnyObject) {
+        let firstName = firstNameTextField.text
+        let lastName = lastNameTextField.text
+        let password = passwordTextField.text
+        let repeatPassword = passwordRepeatTextField.text
+        
+        // Check for empty fields
+        if (firstName!.isEmpty || lastName!.isEmpty || password!.isEmpty || repeatPassword!.isEmpty)
+        {
+            // Display an alert message
+            displayMyAlertMessage("All fields are required")
+            return
+        }
+        
+        // Check if passwords match
+        if password != repeatPassword
+        {
+            // Display an alert message
+            displayMyAlertMessage("Passwords do not match")
+            return
+        }
+        
+        let myUrl = NSURL(string: "http://www.shijianloveslareine.com/scripts/updateProfile.php")
+        let request = NSMutableURLRequest(URL: myUrl!)
+        request.HTTPMethod = "POST"
+        
+        let postString = "firstName=\(firstName)&lastName=\(lastName)"
+        
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            if error != nil {
+                print("error=\(error)")
+                return
+            }
+            
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers) as? NSDictionary
+                
+                if let parseJSON = json {
+                    let resultValue = parseJSON["status"] as! String!
+                    print("result: \(resultValue)")
+                    
+                    let messageToDisplay: String = parseJSON["message"] as! String!
+
+                    
+                    dispatch_async(dispatch_get_main_queue(), {
+                        
+                        let myAlert = UIAlertController(title: "Alert", message: messageToDisplay, preferredStyle: UIAlertControllerStyle.Alert)
+                        
+                        let okAction = UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default) { action in
+                            self.dismissViewControllerAnimated(true, completion: nil)
+                        }
+                        
+                        myAlert.addAction(okAction)
+                        self.presentViewController(myAlert, animated: true, completion: nil)
+                    })
+                }
+                
+            } catch { print(error) }
+            
+        }
+        
+        task.resume()
+    }
+    
+    func displayMyAlertMessage(userMessage: String)
+    {
+        let myAlert = UIAlertController(title: "Alert", message: userMessage, preferredStyle: UIAlertControllerStyle.Alert)
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+        
+        myAlert.addAction(okAction)
+        
+        self.presentViewController(myAlert, animated: true, completion: nil)
     }
 }
